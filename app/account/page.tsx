6348a5de0f7d3f885f35b220952d064c015d333a
Joshua Doodnauth <jsdoodnauth@gmail.com>
2024-09-25 00:15:15 -0400

    add auth redirect

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,11 @@
 import { auth } from "../server/auth";
+import { redirect } from 'next/navigation'
 
 export default async function DashboardPage() {
   const session = await auth();
+  if (!session) {
+    redirect('/')
+  }
 
   if (session) {
     return (
